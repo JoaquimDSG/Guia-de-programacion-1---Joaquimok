3d53681680531383ac6cad343653826115a1a7a4
JoaquimDSG <101809276+JoaquimDSG@users.noreply.github.com>
2022-03-30 20:38:18 -0300

    Hice los Ej 1/2/3/4/5

---
+++ Assets/Scripts/Ejercicio_03.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_03 : MonoBehaviour
+{
+    public string palabra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola "+palabra);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio_05.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_05 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            Debug.Log(num1 + "/" + num2 + " = " + (num1 / num2));
+        }
+        catch
+        {
+            Debug.Log("No se puede dividir un numero por cero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
